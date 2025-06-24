547d6a245cac016171eff0801f0593a346de283e
Eniola Olawumi Farinde <eniolafarinde@gmail.com>
2025-06-24 15:33:55 -0700

    created state management and fetch calls to the backend endpoint

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import logo from '../assets/zuno-logo.png';
 import '../components/SignUp.css'
 
 const SignIn = () => {
+    const [form, setForm] = useState({});
+    const [message, setMessage] = useState("");
+    const [success, setSuccess] = useState(false);
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+    const signin = async (e) => {
+        e.preventDefault();
+        setMessage('');
+        setSuccess(false);
+        if (!form.email || !form.password) {
+            setMessage('Email and password are required.');
+            return;
+        }
+        const response = await fetch("http://localhost:5000/api/auth/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(form)
+        });
+        const data = await response.json();
+        if (response.ok) {
+            setMessage(data.message || `Welcome Back, ${data.user.firstName}!`);
+            setSuccess(true);
+            localStorage.setItem('token', data.token);
+            setForm({ email: '', password: '' });
+            navigate('/');
+
+        } else {
+            setMessage(data.message || "Log-In failed.");
+        }
+    }
+
   return (
     <div>
         <header className="App-header">
@@ -14,18 +51,14 @@ const SignIn = () => {
         </div>
         <div className="sign-up-container">
             <div className="sign-up">
+                {message && ( <p className={`status-message ${success ? 'success' : 'error'}`}> {message}</p>)}
                 <h1>Log In</h1>
-                <form>
-                    <label> Email <input type="email" name="email"/> </label>
-                    <label> Password <input type="password" name="password"/></label>
+                <form onSubmit={signin}>
+                    <label> Email <input type="email" name="email" onChange={handleChange}/> </label>
+                    <label> Password <input type="password" name="password" onChange={handleChange}/></label>
                     <button type="submit" className="signup-btn">Log In</button>
                 </form>
-                <p className="login-link"> Don't have an account? <a href="/">Create an Account</a>
-                </p>
-                <div className="divider">or</div>
-                <button className="google-btn">
-                    Continue with Google
-                </button>
+                <p className="login-link"> Don't have an account? <Link to="/">Create an Account</Link></p>
             </div>
         </div>
     </div>
